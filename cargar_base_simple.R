31854a5e4f0d7c7856da55da4bfec1694f5d8016
Ferneiraks <fer.neiraks@gmail.com>
2025-07-10 21:28:03 -0400

    Update cargar_base_simple.R

--- cargar_base_simple.R
+++ cargar_base_simple.R
@@ -1,3 +1,21 @@
+###install.packages("tidyverse")
+
+library(tidyverse)
+
+library(stringr)
 library(readxl)
 actividad01 <- read_excel("C:/Users/PC/Downloads/actividad01.xlsx")
 View(actividad01)
+
+actividad02 <- read_excel("C:/Users/PC/Downloads/actividad01.xlsx", 
+                          sheet = "variables")
+View(actividad02)
+
+filtro <- filter(actividad01, str_detect(Rapid_rating, "NULL"))
+
+filtro
+b<-actividad01 %>%  mutate(Rapid_rating = str_replace(Rapid_rating, "NULL", ""))
+b
+c<-filter(b, str_detect(Rapid_rating, "NULL"))
+c
+str_replace_na()
