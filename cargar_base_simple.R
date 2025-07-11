ca5f386f93fe7592128bd48c4783b08a930b1573
Ferneiraks <fer.neiraks@gmail.com>
2025-07-10 21:49:59 -0400

    Update cargar_base_simple.R

--- cargar_base_simple.R
+++ cargar_base_simple.R
@@ -11,6 +11,11 @@ actividad02 <- read_excel("C:/Users/PC/Downloads/actividad01.xlsx",
                           sheet = "variables")
 View(actividad02)
 
+arrange_01 = actividad01 %>% 
+  group_by(Blitz_rating) %>% 
+  summarise(n()) %>% 
+  arrange(n())
+
 filtro <- filter(actividad01, str_detect(Rapid_rating, "NULL"))
 
 filtro
@@ -18,4 +23,22 @@ b<-actividad01 %>%  mutate(Rapid_rating = str_replace(Rapid_rating, "NULL", ""))
 b
 c<-filter(b, str_detect(Rapid_rating, "NULL"))
 c
-str_replace_na()
+
+
+
+
+new<-b %>%  mutate(Year_of_birth = str_replace(Year_of_birth, "NO RESPONDE", ""))
+new
+new_3<-filter(new, str_detect(Year_of_birth, "NO RESPONDE"))
+new_3
+
+actividad01<-new %>% mutate(as.numeric(Year_of_birth),as.numeric(Rapid_rating), as.numeric(Blitz_rating) )
+
+
+actividad01 %>%  summary()
+
+filtro_rating = actividad01 %>%  filter(Standard_Rating > 4000)
+
+actividad01 %>% group_by(Gender) %>% summarise(n())
+
+###revent.snapshot()
